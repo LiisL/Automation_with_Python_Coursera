24de3b7250c21fa2acea52f10ed8e4bbc34248a9
Liis Lille <liislille@gmail.com>
2023-08-03 13:28:38 +0300

    Python Crash Course- practice quiz- Hello world!

---
+++ Hello_world.py
@@ -0,0 +1,7 @@
+#Practice Quiz: Hello World
+# Output a message that says "Programming in Python is fun!" to the screen.
+print("Programming in Python is fun!")
+
+#Replace the placeholder___ and calculate the Golden ratio:
+ratio = (1+(5**(1/2)))/2
+print(ratio)
