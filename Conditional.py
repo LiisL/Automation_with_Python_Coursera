97ffbb3e1d9c30bffc8505cdfc4d5f908483a852
Liis Lille <liislille@gmail.com>
2023-08-06 16:24:11 +0300

    Adding excercises with conditionals.

--- Conditional.py
+++ Conditional.py
@@ -22,3 +22,48 @@ exponents = [pow(value, 3) for value in values]
 
 print(values)
 print(exponents)
+#comparison Operators with Strings
+print("a string" == "a string")
+print("4 + 5" == 4 + 5)
+print("rabbit"!= "frog")
+event_city = "Shanghai"
+print(event_city != "Shanghai")
+print("three" == 3
+
+#Logical Operators
+
+print((6*3 >= 18) and (9+9 <= 36/2))
+print("Nairobi" < "Milan" and "Nairobi" > "Hanoi")
+# True or True returns True
+print((15/3 < 2+4) or (0 >= 6-7))
+
+
+# False or True returns True
+country = None
+city = None
+print(country == "New York City" or city == "New York City")
+
+
+# True or False returns True
+print(16 <= 4**2 or 9**(0.5) != 3)
+
+
+# False or False returns False
+print("B_name" > "C_name" or "B_name" < "A_name")
+
+
+#Branching with if Statements
+def hint_username(username):
+    if len(username) < 3:
+        print("Ivalid username! Username must be atleast 3 characters long. ")
+        
+hint_username("Li")
+
+#else Statements
+def hint_username(username):
+    if len(username) < 3:
+        print("Ivalid username! Username must be atleast 3 characters long. ")
+    else:
+        print("valid username!")
+        
+hint_username("Lii")        
