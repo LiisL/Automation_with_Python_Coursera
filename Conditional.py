d50a65d4df38a86f04abdebbc364da966c085745
Liis Lille <liislille@gmail.com>
2023-08-10 14:26:24 +0300

    Adding new exercises to Conditionals.

--- Conditional.py
+++ Conditional.py
@@ -124,3 +124,50 @@ print(your_stage_name("Angelina"))
 print(your_stage_name("Brianne")) 
 print(your_stage_name("Lovely"))       
 
+# Sets value of the "number" variable
+number = 25
+
+# The "number" variable will first be compared to 5. Since it is 
+# False that "number" is not less than or equal to 5, the expression indented 
+# under this line will be ignored. 
+if number <= 5: 
+   print("The number is 5 or smaller.")
+ 
+# Next, the "number" variable will be compared to 33. Since it is 
+# False that "number" is equal to 33, the expression indented under 
+# this line will be ignored. 
+elif number == 33:
+   print("The number is 33.")
+ 
+# Then, the "number" variable will be compared to 32 and 6. Since it 
+# is True that 25 is less than 32 and greater than 6, the Python 
+# interpreter will print "The number is less than 32 and/or greater
+# than 6." Then, it will exit the if-elif-else statement and the remainder 
+# of the if-elif-else statement will be ignored.
+elif number < 32 and number >= 6:
+   print("The number is less than 32 and greater than 6.")
+ 
+else:
+   print("The number is " + str(number))
+ 
+# Expected output is: 
+# The number is less than 32 and greater than 6.
+
+# This function rounds a variable number up to the nearest 10x value
+def round_up(number):
+  x = 10
+# The floor division operator will calculate the integer value of
+# "number" divided by x: 35 // 10 will return the integer 3.
+  whole_number = number // x
+# The modulo operator will calculate the remainder value of "number"
+# divided by x: 35 % 10 will return the remainder value 5.
+  remainder = number % x
+# If the remainder is greater than 0: 
+  if remainder >= 5: 
+# Return x multiplied by the (whole_number+1) to round up
+    return x*(whole_number+1)
+# Else, return x multiplied by the whole_number to round down
+  return x*whole_number
+ 
+# Calls the function with the parameter value of 35.
+print(round_up(35)) # Should print 40
